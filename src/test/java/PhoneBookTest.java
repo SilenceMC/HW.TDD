795cd65552a23c694fa800a07d307d6975687802
Artem Chernyshov <silence_made@mail.ru>
2023-09-14 19:39:21 +0300

    Stub and tests for add

--- src/test/java/PhoneBookTest.java
+++ src/test/java/PhoneBookTest.java
@@ -1,3 +1,22 @@
-public class PhoneBookTest {
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
+public class PhoneBookTest {
+    PhoneBook phoneBook = new PhoneBook();
+
+    private final String testName = "Семен";
+    private final String testPhoneNumber = "+7(999)1002525";
+
+    @Test
+    public void addTest(){
+        //arrange
+        int expected = 1;
+
+        //act
+        int result = phoneBook.add(testName,testPhoneNumber);
+
+        //assert
+        Assertions.assertEquals(expected, result);
+    }
 }
